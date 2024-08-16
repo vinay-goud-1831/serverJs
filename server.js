9c66f4c458c7b0d46a0522ea681e836838d5d5c8
vinay goud ganapuram <85302849+vinay-goud-1831@users.noreply.github.com>
2024-08-16 08:43:17 +0000

    demo

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const port = 8081;
 
 http.createServer((req,res)=> {
     res.writeHead(400,{"content-type":"text/html"});
-    res.write("<h2> hey server started :-)</h2>");
+    res.write("<h2> hey server started :-)134</h2>");
     res.end();
 })
 .listen(port, () => {
-    console.log(`Nodejs server started running on port ${port}`);
+    console.log(`Nodejs server started running on port 123 ${port}`);
 });
