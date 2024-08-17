0c737c9019351b1c65bce963a063a9ae7f7f610f
vinay goud ganapuram <85302849+vinay-goud-1831@users.noreply.github.com>
2024-08-17 10:23:57 +0000

    nodeJs

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
-const http = require("http");
-const port = 8081;
 
-http.createServer((req,res)=> {
-    res.writeHead(400,{"content-type":"text/html"});
-    res.write("<h2> hey server started :-)134</h2>");
-    res.end();
+const http = require("http");
+
+const port = 8081 ;
+
+const toDOList = ["vinay" , "goud ", "ganapuram"];
+
+http
+.createServer((req,res)=>{
+    const {method , url} = req;
+
+if (url === "/todos"){
+    if(method === "GET"){
+        res.writeHead(200,{"Content-Type" : "text/html"});
+        res.write(toDOList.toString());
+
+    }
+    else if(method === "POST"){
+        let data = "";
+        req
+        .on( "error",(err) =>{
+           
+
+        }).on("data",(chunck)=>{
+            data += chunck ;
+           
+        }).on("end",()=>{
+
+            
+            body = JSON.parse(data);
+
+            let newtoDO = toDOList ;
+
+            newtoDO.push(body.item);  
+        });
+
+    } else{
+        res.writeHead(501);
+
+         } 
+    
+    }
+    else{
+        res.write(404);
+        }
+
+        res.end();
 })
-.listen(port, () => {
-    console.log(`Nodejs server started running on port 123 ${port}`);
+.listen(port, ()=> {
+    console.log(`it was excuted on port 8081 ${port} `);
 });
+
+
+
